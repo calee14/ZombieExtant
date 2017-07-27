3bae8ef8423b82c1fb60a01845ca054bc4d89e7c
calee14 <caplee1000@gmail.com>
2017-07-27 15:07:39 -0700

    Finished wave design and spacing between them

--- ZombieExtant/GameScene.swift
+++ ZombieExtant/GameScene.swift
@@ -31,7 +31,15 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     var fixedDelta: CFTimeInterval = 1.0/60.0 // 60 FPS
     var toBeDeleted: [SKSpriteNode] = [SKSpriteNode]()
     var contactTimer: CFTimeInterval = 0
-    var spawnTimer: CFTimeInterval = 1.4
+    var spawnTimer: CFTimeInterval = 0
+    var stagerTimer: UInt32 = 6
+    var waveNum = 1
+    var zombieCount = 0
+    var zombieSpawned = 0
+    var zombiesAddedEveryWave = 24
+    var zombieOnGroundCount = 0
+    var waveBegan = false
+    var filterZombiesPerWave = 24
     
     override func didMove(to view: SKView) {
         //Set up scene here
@@ -57,8 +65,6 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         for turret in turretLayer.children as! [TopGun] {
             turret.gameScene = self
         }
-        
-        spawnWave(wave: 1)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -130,6 +136,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 toBeDeleted.append(nodeA as! SKSpriteNode)
                 toBeDeleted.append(nodeB as! Bullet)
                 
+                //Update zombie count
+                zombieCount -= 1
+                zombieOnGroundCount -= 1
+                //Disable the bullet
                 bullet.bulletHit = true
                 return
             } else if nodeB.name == "zombie" && nodeA.name == "bullet" {
@@ -138,6 +148,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 if bullet.bulletHit != false { return }
                 toBeDeleted.append(nodeB as! SKSpriteNode)
                 toBeDeleted.append(nodeA as! Bullet)
+                
+                //Update zombie count
+                zombieCount -= 1
+                zombieOnGroundCount -= 1
+                //Disable the bullet
                 bullet.bulletHit = true
                 return
             }
@@ -147,6 +162,10 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
+        print("zombie count = \(zombieCount)")
+        
+        //Manages the waves
+        waveManager()
         
         //removes the objects that need to be removed
         for object in toBeDeleted {
@@ -166,12 +185,79 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             }
             print("remove")
         }
+        
+        //Update timers
+        //spawnTimer += fixedDelta
+    }
+    
+    func waveManager() {
+        if waveBegan == false {
+            //Gets the value of the num of zombies in the wave
+            zombieCount = waveNum * zombiesAddedEveryWave
+            if waveNum >= 5 {
+                //96 is four 24 * num of spawners which is 4
+                //4 is for every level after 5
+                zombieCount = (waveNum - 4) * 96
+            } else if waveNum < 5 {
+                zombieCount = waveNum * zombiesAddedEveryWave
+            }
+            waveBegan = true
+        }
+        
+        var totalNumOfZom = 0 //waveNum * zombiesAddedEveryWave
+        if waveNum >= 5 {
+            //96 is four 24 * num of spawners which is 4
+            //4 is for every level after 5
+            totalNumOfZom = (waveNum - 4) * 96
+        } else if waveNum < 5 {
+            totalNumOfZom = waveNum * zombiesAddedEveryWave
+        }
+        
+        if zombieSpawned < totalNumOfZom {
+            print("zombies \(zombieSpawned) count \(zombieOnGroundCount)")
+            //Run code to add more zombies
+            //Check if it is the right time to spawn zombies
+            if zombieOnGroundCount <= zombieSpawned / 4 {
+                spawnWave(wave: waveNum)
+            }
+
+        } else if zombieCount <= 0 {
+            //if we killed all the zombies start a new wave
+            //make a new wave
+            //Update the zombie spawned
+            
+            //let wait = SKAction.wait(forDuration: 5)
+            zombieSpawned = 0
+            zombieOnGroundCount = 0
+            waveBegan = false
+            //Start a new wave
+            waveNum += 1
+            spawnWave(wave: waveNum)
+            print("wave count = \(waveNum)")
+        }
     }
     
     func spawnWave(wave: Int) {
-        let zombiesPerWave = wave * 100
-        let zombiesPerSpawner = zombiesPerWave / 4
-        if spawnTimer > 1 {
+        //Get the total num zombies for the wave
+        var zombiesPerWave = 0
+        if wave >= 5 {
+            //96 is four 24 * num of spawners which is 4
+            //4 is for every level after 5
+            zombiesPerWave = (wave - 4) * 96
+        } else if wave < 5 {
+            zombiesPerWave = 24
+        }
+         //+ zombieLayer.children.count
+        var zombiesPerSpawner = Int(zombiesPerWave / 4)
+        //Filter the num of zombies per spawner
+        if zombiesPerSpawner > filterZombiesPerWave {
+            zombiesPerSpawner = filterZombiesPerWave
+        }
+        //Update the spawned num of zombies
+        zombieSpawned += zombiesPerSpawner * 4
+        zombieOnGroundCount += zombiesPerSpawner * 4
+        
+        //if spawnTimer > 1 {
             //Top spawn
             addZombies(count: zombiesPerSpawner, spawner: topSpawn)
             //Right spawn
@@ -180,56 +266,60 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             addZombies(count: zombiesPerSpawner, spawner: leftSpawn)
             //Bottom spawn
             addZombies(count: zombiesPerSpawner, spawner: bottomSpawn)
-        }
+            //spawnTimer = 0
+        //}
         
         
     }
     
     func addZombies(count: Int, spawner: SKSpriteNode) {
-        //Using the four spawners around the
-        //if spawner.name == "topSpawn" || spawner.name == "rightSpawn" {
-            var distanceCounter: CGFloat = 0
-            var randPosition = CGPoint()
-            for _ in 0...count {
-                if spawner.name == "topSpawn" {
-                    //Create the topSpawn rand position
-                    let randX = CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width)))
-                    let randY = CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height))) + spawner.position.y
-                    randPosition = CGPoint(x: randX, y: randY)
-                } else if spawner.name == "rightSpawn" {
-                    //Create the right rand position
-                    let randX = CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width))) + spawner.position.x + distanceCounter
-                    let randY = CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height)))
-                    randPosition = CGPoint(x: randX, y: randY)
-                } else if spawner.name == "leftSpawn" {
-                    //Create the leftSpawn rand position
-                    let randX = -1 * (CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width)))) - distanceCounter
-                    let randY = CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height)))
-                    randPosition = CGPoint(x: randX, y: randY)
-                } else if spawner.name == "bottomSpawn" {
-                    //Create the bottomSpawn rand position
-                    let randX = CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width)))
-                    let randY = -1 * (CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height)))) - CGFloat(arc4random_uniform(50))
-                    randPosition = CGPoint(x: randX, y: randY)
-                }
-                //Creates a number of zombies
-                if distanceCounter > 50 { distanceCounter = 0 }
-//                //Creates a random position
-//                let randX = CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width))) //+ spawner.position.x
-//                let randY = CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height)))// + spawner.position.y
-//                let position = CGPoint(x: CGFloat(randX), y: CGFloat(randY))
-                //Add a zombie to the scene
-                let newZombie = Zombie()
-                newZombie.gameScene = self
-                self.zombieLayer.addChild(newZombie)
-                newZombie.spd = Double(arc4random_uniform(4)) + 20.0
-                newZombie.position = randPosition
-                //Make the zombie move
-                newZombie.moveToClosestTurret()
-                //Update the distance Counter
-                distanceCounter += 1
+        //Using the four spawners around the map
+        //Create an offset
+        var randPosition = CGPoint()
+        for _ in 1...count {
+            let distanceCounter: CGFloat = CGFloat(arc4random_uniform(200))
+            print("rand num = \(distanceCounter)")
+            //if spawnTimer > 0.2 {
+            if spawner.name == "topSpawn" {
+                //Create the topSpawn rand position
+                let randX = CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width)))
+                let randY = CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height))) + spawner.position.y
+                randPosition = CGPoint(x: randX, y: randY)
+            } else if spawner.name == "rightSpawn" {
+                //Create the right rand position
+                let randX = CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width))) + spawner.position.x + distanceCounter
+                let randY = CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height)))
+                randPosition = CGPoint(x: randX, y: randY)
+            } else if spawner.name == "leftSpawn" {
+                //Create the leftSpawn rand position
+                let randX = -1 * (CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width)))) - distanceCounter / 4
+                let randY = CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height))) + distanceCounter / 4
+                randPosition = CGPoint(x: randX, y: randY)
+            } else if spawner.name == "bottomSpawn" {
+                //Create the bottomSpawn rand position
+                let randX = CGFloat(arc4random_uniform(UInt32(spawner.position.x + spawner.size.width))) + distanceCounter / 4
+                let randY = -1 * (CGFloat(arc4random_uniform(UInt32(spawner.position.y + spawner.size.height)))) - CGFloat(distanceCounter)
+                randPosition = CGPoint(x: randX, y: randY)
             }
+            //Creates a number of zombies
+            //Add a zombie to the scene
+            let newZombie = Zombie()
+            newZombie.gameScene = self
+            newZombie.spd = 20.0 //Double(arc4random_uniform(4)) + 20.0
+            newZombie.position = randPosition
+            //Make the zombie move
+            newZombie.moveToClosestTurret()
+            //Update the distance Counter
+            let wait = SKAction.wait(forDuration: TimeInterval(arc4random_uniform(stagerTimer)))
+            let addZombie = SKAction.run({ [unowned self] in
+                self.zombieLayer.addChild(newZombie)
+            })
+            let seq = SKAction.sequence([wait, addZombie])
+            self.run(seq)
+        }
+        //}
     }
+    
     func distanceTo(_ dist1: CGPoint, _ dist2: CGPoint) -> CGFloat {
         //Finds the distance between two positions
         let xDist = dist1.x - dist2.x
